051f1060d674b925491262eb065d0606cf60a25e
boshiro123 <b.shiroki@mail.ru>
2024-06-26 10:36:44 +0300

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {PizzaComponent} from "../pizza/pizza.component";
+import {PizzaComponent} from "./pages/pizza/pizza.component";
 
 @Component({
   selector: 'app-root',
