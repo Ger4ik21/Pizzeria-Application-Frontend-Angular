21ff134005b4ddf448dfaded836610b0498d8d21
boshiro123 <b.shiroki@mail.ru>
2024-06-29 13:34:34 +0300

    Add new-category validation

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../../common-ui/header/header.component";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
+import {Category} from "../../data/intefaces/category.interface";
 
 @Component({
   selector: 'app-admin',
   standalone: true,
     imports: [
-        HeaderComponent
+        HeaderComponent,
+        FormsModule,
+        NgIf,
+        ReactiveFormsModule
     ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
 
+    category!: Category
+    categoryForm= new FormGroup({
+        category: new FormControl(null,[Validators.required,Validators.minLength(3)])
+    })
+    addNewCategory(){
+        console.log(this.categoryForm.controls['category'].value);
+    }
 }
